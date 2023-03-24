6c57218a8c3bde61cf758dda41b5f6f221dd327c
zhangrui <zhangrui@126.com>
2023-03-24 19:12:58 +0800

    提交代码

--- chap04/samp4_8/mainwindow.cpp
+++ chap04/samp4_8/mainwindow.cpp
@@ -6,6 +6,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    /*
+     * 将ScrollArea组件设置为主窗口工作区的中心组件后，
+     * DockWidget与ScrollArea之间自动出现分割条，可以分割两个组件的大小
+     */
+    setCentralWidget(ui->scrollArea);
 }
 
 MainWindow::~MainWindow()
