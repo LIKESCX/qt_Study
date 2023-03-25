1481bcd31440eb593ce532bf1d2863f97eabac2e
unknown <scx@126.com>
2023-03-25 12:06:43 +0800

    提交代码

--- chap04/samp4_8/mainwindow.cpp
+++ chap04/samp4_8/mainwindow.cpp
@@ -11,9 +11,29 @@ MainWindow::MainWindow(QWidget *parent) :
      * DockWidget与ScrollArea之间自动出现分割条，可以分割两个组件的大小
      */
     setCentralWidget(ui->scrollArea);
+    initTree();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::initTree()
+{//初始化目录树
+    QString dataStr = "";   //Item 的Data 存储的string
+    ui->treeFiles->clear();
+    QIcon icon;
+    icon.addFile(":/images/icons/15.ico");
+
+    QTreeWidgetItem* item = new QTreeWidgetItem(MainWindow::itTopItem);
+    item->setIcon(MainWindow::colItem,icon);  //第1列的图标
+    item->setText(MainWindow::colItem,"图片文件"); //第1列的文字
+    item->setText(MainWindow::colItemType,"type=itTopItem"); //第2列
+    item->setFlags(Qt::ItemIsSelectable | Qt::ItemIsUserCheckable | Qt::ItemIsEnabled | Qt::ItemIsTristate);
+    item->setCheckState(colItem,Qt::Checked);
+
+    item->setData(MainWindow::colItem, Qt::UserRole, QVariant(dataStr));
+    ui->treeFiles->addTopLevelItem(item);//添加顶层节点
 }
--- chap04/samp4_8/mainwindow.h
+++ chap04/samp4_8/mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QLabel>
+#include <QTreeWidgetItem>
 namespace Ui {
 class MainWindow;
 }
@@ -17,6 +18,52 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+private:
+    //枚举类型treeItemType，创建节点时用作type参数，自定义类型必须大于1000
+    enum treeItemType {
+       itTopItem = 1001,
+       itGroupItem,
+       itImageItem
+    };
+    //目录树列的编号
+    enum treeColNum {
+        colItem = 0,
+        colItemType = 1
+    };
+
+    //用于状态栏文件名显示
+    QLabel *LabFileName;
+    //当前的图片
+    QPixmap curPixmap;
+    //当前图片缩放比例
+    float pixRatio;
+
+    //目录树初始化
+    void initTree();
+    //添加目录
+    void addFolderItem(QTreeWidgetItem *parItem, QString dirName);
+    //提取目录名称
+    QString getFinalFolderName(const QString &fullPathName);
+    //添加图片
+    void addImageItem(QTreeWidgetItem *parItem, QString aFileName);
+    //显示一个图片节点的图片
+    void displayImage(QTreeWidgetItem *item);
+    //遍历改变节点标题
+    void changeItemCaption(QTreeWidgetItem *item);
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
 
 #endif // MAINWINDOW_H
