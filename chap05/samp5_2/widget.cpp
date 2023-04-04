3b759d9b00250f6f4519a7977d07422f5af07ac4
zhangrui <zhangrui@126.com>
2023-04-04 18:35:07 +0800

    提交代码

--- chap05/samp5_2/widget.cpp
+++ chap05/samp5_2/widget.cpp
@@ -10,7 +10,8 @@ Widget::Widget(QWidget *parent) :
     theStrList << "北京" << "上海" << "天津" << "广州" << "深圳" << "郑州" << "成都";
     theModel = new QStringListModel(this);
     theModel->setStringList(theStrList);//导入theStrList的内容
-    ui->listView->setModel(theModel);
+    ui->listView->setModel(theModel);//设置数据模型
+    ui->listView->setEditTriggers(QAbstractItemView::DoubleClicked | QAbstractItemView::SelectedClicked);
 }
 
 Widget::~Widget()
